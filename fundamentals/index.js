c8602df0f9466636bbac304bfb38ca52155fddde
Emad-Ahmed6699 <Emad.Elden2974@compit.aun.edu.eg>
2026-01-31 21:02:11 +0200

    fs and stream modules

--- fundamentals/index.js
+++ fundamentals/index.js
@@ -114,33 +114,104 @@ event.emit('click');// emit an event --> call all the listeners
 
 //example sign up 
 let users = []
-event.on('sendmail' , async (email ) => {
+event.on('sendmail', async (email) => {
     console.log(`email received `);
     await Sendmail(email);
     console.log(`email done `);
 })
-async function SignUp(email , password) {
+/*async*/ function SignUp(email, password) {
     users.push({
         email,
         password
     });
     //await Sendmail(email);//stay untill the function is finished including the timeout
-    event.emit('sendmail' , email);
+
+    //but we need to be display the users to the frontend ,then five second to send the email 
+    //so we use event.emit('sendmail' , 'emad')
+    event.emit('sendmail', email);
     console.log(users); //excuted after 5 seconds 
 }
 function Sendmail(email) {
-    return new Promise((r , j) => { // use promise when you want to wait for something
+    return new Promise((r, j) => { // use promise when you want to wait for something
         setTimeout(() => { // method that will be executed after 5 seconds (metheod in setTimeout is a callback function)
             console.log(`email sent to ${email}`);
             r();//resolve : mean that the promise is resolved
         }, 5000);//5 seconds
     })
 }
-   SignUp('emad' , '123456');  //here we stay untill the function is finished including the timeout 
-//but we need to be display the users to the frontend ,then five second to send the email 
-//so we use event.emit('sendmail' , 'emad')
+ /*SignUp('emad' , '123456');*/  //(((((call the function to understand the code))))))
 
 
 
-  //***the code by default is synchronous but we can make it asynchronous by using promises and async await***
-  //
+
+
+//***the code by default is synchronous but we can make it asynchronous by using promises and async await***
+
+
+
+console.log("-----------------------------------------");
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//fs module
+
+const fs = require('node:fs');
+//to read a file we need a path
+const path2 = require('node:path');
+const sourcepath = path2.resolve('./data.txt');//resolve the relative path to an absolute path
+console.log(sourcepath);
+const filedata = fs.readFileSync(sourcepath, {encoding:'utf-8' , flag : 'r'});//read the file
+console.log(filedata);                     //{encoding:'utf-8' , flag : 'r'} is options as object so we use {}
+//to write a file we need a path
+const destinationpath = path2.resolve('./data2.txt');
+console.log(destinationpath);
+fs.writeFileSync(destinationpath, 'hello world' , {flag : 'w'});//write the file
+
+//to append a file we need a path
+const destinationpath2 = path2.resolve('./data2.txt');
+console.log(destinationpath2);
+fs.appendFileSync(destinationpath2, '\nhello world 2' , {flag : 'a'});//append the file
+
+
+
+console.log("*-----------------------------------------");
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//stream module (video or audio) divid the file into chunks and send them to the server
+
+//if we have big data in data.text ,if we use the fs module then the RAM will be full so we use the stream module
+
+const fs2= require('node:fs');
+const path3 = require('node:path');
+const sourcepath2 = path3.resolve('./data.txt');
+const readablestream = fs2.createReadStream(sourcepath2);
+readablestream.on('data', (chunk) => { //this event is called when we receive data 
+    console.log('data received');
+    console.log(chunk); //each chunk is a buffer with 64kb
+    console.log(chunk.toString());
+    console.log('data done');
+})
+
+readablestream.on('end', () => { //this event is called when we finish reading the file
+    console.log('file done');
+})
+
+readablestream.on('error', (err) => { //this event is called when we have an error
+    console.log(err);
+})
+
+//to write a file we need a path
+const destinationpath3 = path3.resolve('./data3.txt');
+const writablestream = fs2.createWriteStream(destinationpath3);
+writablestream.write('hello world');
+writablestream.end();
+
+
+
+console.log("*-----------------------------------------");
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//http module
+const http = require('node:http');
